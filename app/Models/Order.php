2154e7fda39145ef38d5cb4910032e75846cd0f6
moannn27 <naotomori456@gmail.com>
2025-05-06 21:52:49 +0700

    OrderResourch

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -25,8 +25,8 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function item(){
-        return $this->hasMany(Orderitem::class);
+    public function items(){
+        return $this->hasMany(OrderItem::class);
     }
 
     public function address(){
