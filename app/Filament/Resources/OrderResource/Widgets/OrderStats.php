09360556d268a72b8bea159323876b94d7754634
moannn27 <naotomori456@gmail.com>
2025-05-07 15:09:01 +0700

    menambah list order dan orderstats

---
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class OrderStats extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('New Orders',Order::query()->where('status', 'new')->count()),
+            Stat::make('Order Processing', Order::query()->where('status', 'processing')->count()),
+            Stat::make('Order Shipped', Order::query()->where('status', 'Shipped')->count()),
+            Stat::make('Average Price', (Order::query()->avg('grand_total')),'In IDR'),
+        ];
+    }
+}
