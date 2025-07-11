813c73ad99af7a11609b694376807299c9218db9
moannn27 <naotomori456@gmail.com>
2025-07-11 15:17:02 +0700

    menambahkan tailwind css

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-4xl text-red-400">Welcome to the Home Page</h1>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   darkMode: 'class', // Enable dark mode support
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ }
