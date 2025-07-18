72823736cb18eac5e16c41cc97e74d59dcda0a3e
moannn27 <naotomori456@gmail.com>
2025-07-18 16:35:44 +0700

    home dan kategori jadi dinamik

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
-class CategoriesPage extends Component
-{
-    public function render()
-    {
-        return view('livewire.categories-page');
+#[Title('Categories - Platinum Computer')]
+class CategoriesPage extends Component{
+    public function render(){
+        $categories = \App\Models\Category::where('is_active',1)->get();
+        return view('livewire.categories-page', [
+            'categories' => $categories,
+        ]);
     }
 }
--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
-class HomePage extends Component
-{
-    public function render()
-    {
-        return view('livewire.home-page');
+
+    #[Title('Home Page - Platinum Computer')]
+    class HomePage extends Component {
+    public function render(){
+        $brand = Brand::where('is_active',1)->get();
+        $categories = \App\Models\Category::where('is_active',1)->get();
+        return view('livewire.home-page', [
+            'brands' => $brand,
+            'categories' => $categories,
+        ]);
     }
 }
+
